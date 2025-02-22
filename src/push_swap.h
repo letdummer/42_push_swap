5ed778269571a5d7f6207228aef4a5a89546d995
Leticia Dummer <ldummer-@student.42porto.com>
2025-02-22 15:04:26 +0000

    fix path to libft.h

--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ldummer- <ldummer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 20:13:20 by ldummer-          #+#    #+#             */
-/*   Updated: 2025/02/22 00:00:54 by ldummer-         ###   ########.fr       */
+/*   Updated: 2025/02/22 15:03:10 by ldummer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 /*  						 LIBRARYS	                                      */
 /* ************************************************************************** */
 
-# include "../libft/libft/libft.h"
+# include "../libft/libft.h"
 # include "../ft_printf/libftprintf.h"
 # include <unistd.h>			// read()	/	write()
 # include <stdlib.h>			// malloc()	/	free()	/ exit()
